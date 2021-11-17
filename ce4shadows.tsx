409f8e8d7b166e8644b7cb988039472245f62c67
Micha Umlauft <micha@umlauft-online.de>
2021-11-17 20:01:26 +0100

    Night v2 (integriert)

--- ce4shadows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="ce4shadows" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
- <image source="c4e_shadows.png" width="512" height="2048"/>
-</tileset>
